68b61bc59dc5bde2ecc14bba34b5f733c0006058
Lohn IMAI <mail@lohn.me>
2017-07-03 16:47:18 +0900

    Move eslint configs from tapplint

---
+++ rules/nodejs-and-commonjs.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    // require error handling in callbacks
+    'handle-callback-err': 'error',
+
+    // disallow require calls to be mixed with regular variable declarations
+    'no-mixed-requires': 'error',
+
+    // disallow new operators with calls to require
+    'no-new-require': 'error'
+  }
+};
---
+++ rules/variables.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    // disallow catch clause parameters from shadowing variables in the outer scope
+    'no-catch-shadow': 'error',
+
+    // disallow deleting variables
+    'no-delete-var': 'error',
+
+    // disallow labels that share a name with a variable
+    'no-label-var': 'error',
+
+    // disallow the use of undeclared variables unless mentioned in /*global */ comments
+    'no-undef': 'error',
+
+    // disallow unused variables
+    'no-unused-vars': 'error',
+
+    // disallow the use of variables before they are defined
+    'no-use-before-define': 'error'
+  }
+};
